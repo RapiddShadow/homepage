42bb32b6d4e3cd96298cfebd5229c6e67f94cb66
Siddharth Dalvi <8900sid@gmail.com>
2025-03-02 19:28:08 -0600

    Update

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,8 +4,9 @@ const WelcomeMessage = () => {
         <div className="welcome-div"><h1 className="welcome-h1">Hello, <br/> my name is Siddharth</h1>
         
         <br />
-        <p>I am a computer engineer pursuing MS in Computer Science and I am interested in web development!
-            Refresh the website for different particle colors in the background
+        <p>I am a tech enthusiast who holds an MS in Computer Science and a BE in Computer Engineering. 
+            This website demonstrates some of my skills. I have used Three.js to make this website fun while also maintaining accessibility. 
+            You can refresh the page to change background color!
         </p>
         </div>
     )
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,9 +1,10 @@
 import video from "../assets/welcome.mp4"
 
-
 const VideoComp = () => {
     return(
-        <video className="videos" src={video} autoPlay loop muted />
+        <video className="videos" src={video} autoPlay loop muted aria-label="Introduction video by Siddhart, typing in a code editor">
+            <track kind="captions" src="/captions.vtt" default />
+        </video>
     )
 }
 
