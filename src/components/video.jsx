bf4afec10a45bce4c3a30f84697c4602d1f9c344
Siddharth Dalvi <8900sid@gmail.com>
2025-03-07 15:28:55 -0600

    update to captions

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -3,7 +3,7 @@ import video from "../assets/welcome.mp4"
 const VideoComp = () => {
     return(
         <video className="videos" src={video} autoPlay loop muted aria-label="Introduction video by Siddhart, typing in a code editor">
-            <track kind="captions" src="../public/captions.vtt" default srclang="en" label="English"/>
+            <track kind="captions" src="captions.vtt" default srclang="en" label="English"/>
         </video>
     )
 }
